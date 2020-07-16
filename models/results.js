e89e3fede6b9aca1d03ed61035a9251d6f7fc066
Robert Garza <robertgarza@Roberts-MacBook-Pro.local>
2020-07-15 23:02:51 -0500

    updated schema and seeds files

--- models/results.js
+++ models/results.js
@@ -5,26 +5,3 @@ module.exports = function(sequelize, DataTypes) {
   });
   return packingList;
 };
-
-//-List of items needed for trip
-
-//-Table: Pack Items
-
-//-Columns:
-//Clothes: underwear, sock, shorts, pants, shrits:short,long,tank, shoes
-
-//Toiletries: Toothbrush, Toothpaste, Shampoo, Conditioner, Soap/Body Wash, Deodorant, Razor, Shaving Cream, Lotion, Sunscreen
-
-//Food: Snacks, Drinks, Cooler, Libations
-
-//Medicine: Prescriptions, Vitamans, Pain Reliever, First Aid Kit, Face Mask, Hand Sanitizer
-
-//Tech Stuff: Phone, Phone Charger, Headphones, Laptop, iPad
-
-//Documents: ID, Passport, Tickets, Wallet
-
-//Female
-//Panties, feminine hygiene products
-
-//Formal Events
-//Formal Wear
--- schema.sql
+++ schema.sql
@@ -2,3 +2,70 @@
 DROP DATABASE IF EXISTS passport_demo;
 -- Creates the "blogger" database --
 CREATE DATABASE passport_demo;
+
+USE passport_demo;
+
+CREATE TABLE clothes
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+   item VARCHAR
+    (100),
+   PRIMARY KEY
+    (id)
+);
+
+    CREATE TABLE toiletries
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
+   item VARCHAR
+        (100),
+   PRIMARY KEY
+        (id)
+);
+
+        CREATE TABLE food
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+   item VARCHAR
+            (100),
+   PRIMARY KEY
+            (id)
+);
+
+            CREATE TABLE medicine
+            (
+                id INT NOT NULL
+                AUTO_INCREMENT,
+   item VARCHAR
+                (100),
+   PRIMARY KEY
+                (id)
+);
+
+                CREATE TABLE tech_stuff
+                (
+                    id INT NOT NULL
+                    AUTO_INCREMENT,
+   item VARCHAR
+                    (100),
+   PRIMARY KEY
+                    (id)
+);
+
+                    CREATE TABLE documents
+                    (
+                        id INT NOT NULL
+                        AUTO_INCREMENT,
+   item VARCHAR
+                        (100),
+   PRIMARY KEY
+                        (id)
+);
+
+
+
+
+
